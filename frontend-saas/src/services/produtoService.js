4980306b178ad237efd44a6ea27bfeb09bd539e8
neneca-lab <hu.patatyy@gmail.com>
2025-06-13 11:34:47 -0300

    serivce

--- frontend-saas/src/services/produtoService.js
+++ frontend-saas/src/services/produtoService.js
@@ -5,7 +5,7 @@ export const listarProdutos = () => api.get("/produto/listar");
 export const cadastrarProduto = (nome, user_id, clienteId) => {
     return api.post(`/produto/cliente/${user_id}/produtos`,  {
         nome,
-        clienteId,
+    
     })
 }
 
