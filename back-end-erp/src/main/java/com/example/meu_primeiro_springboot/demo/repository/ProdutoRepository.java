0b3f3cba71aa9d53e2322cd5cc91ca4a53d7de7b
neneca-lab <hu.patatyy@gmail.com>
2025-06-16 16:50:20 -0300

    find by repository

--- back-end-erp/src/main/java/com/example/meu_primeiro_springboot/demo/repository/ProdutoRepository.java
+++ back-end-erp/src/main/java/com/example/meu_primeiro_springboot/demo/repository/ProdutoRepository.java
@@ -5,9 +5,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
     List<Produto> findByUsuario_Id(Long usuarioId);
+    Optional<Produto> findById(Long usuarioId);
     void deleteAll();
 }
